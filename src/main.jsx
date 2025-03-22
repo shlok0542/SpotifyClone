b9c5de87d0cc2edb521c11491a7a2dbfc01e77ca
shlok0542 <shlokmaury0542a376@gmail.com>
2025-03-22 16:05:59 +0530

    refactor imports in main.jsx for consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import {BrowserRouter} from 'react-router-dom'
-import PlayerContextProvider from './Context.jsx/PlayerContext.jsx'
+import PlayerContextProvider from './Context.jsx/PlayerContext'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
